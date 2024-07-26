3a7583fdb984fb1e9639c00c6e5af49fe26ae031
rahulnayak85 <rahulnayak.p@gmail.com>
2024-07-26 20:43:15 +0100

    Initial commit

---
+++ asteriskPyramidGenerator.js
@@ -0,0 +1,25 @@
+let rows = []
+inverted = true;
+const count = 8;
+
+let character = "*"
+
+function pyramid(rowNumber, rowCount) {
+		return " ".repeat(count - rowNumber) + character.repeat(2*rowNumber - 1) + " ".repeat(rowCount - rowNumber)
+	}
+
+for (let i = 1; i <= count; i++) {
+	if (inverted) {
+		rows.unshift(pyramid(i, count));
+	}
+	else {
+		rows.push(pyramid(i, count))
+	}
+}	
+
+pyramid(count);
+let result = ""
+for (const row of rows) {
+	result = result + "\n" + row; 
+}
+console.log(result);
